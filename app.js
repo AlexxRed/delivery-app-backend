82f38984f58775e4ef7f56e5dd13ec9be5eb15df
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-07-15 18:29:21 +0300

    added route

--- app.js
+++ app.js
@@ -5,27 +5,27 @@ const cors = require('cors')
 const dotenv = require('dotenv');
 dotenv.config()
 
-// const authRouter = require('./routes/api/auth')
+const authRouter = require('./routes/api/auth')
 // const shopsRouter = require('./routes/api/shops')
 
 const app = express()
 
-// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-// app.use(logger(formatsLogger))
-// app.use(cors())
-// app.use(express.json())
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
 
-// app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter)
 // app.use('/api/shops', shopsRouter)
 
-// app.use((req, res) => {
-//   res.status(404).json({ message: 'Not found' })
-// })
+app.use((req, res) => {
+    res.status(404).json({ message: 'Not found' })
+})
 
-// app.use((err, req, res, next) => {
-//   res.status(500).json({ message: err.message })
-// })
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: err.message })
+})
 
 module.exports = app
 
---
+++ models/user.js
@@ -0,0 +1,68 @@
+const { Schema, model } = require("mongoose")
+const Joi = require("joi");
+
+const emailRegexp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Name is required'],
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+        },
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            match: emailRegexp,
+            unique: true,
+        },
+        subscription: {
+            type: String,
+            enum: ["starter", "pro", "business"],
+            default: "starter"
+        },
+        token: {
+            type: String,
+            default: '',
+        },
+        avatarURL: {
+            type: String,
+            required: true,
+        },
+    },
+    { versionKey: false, timestamps: true },
+)
+
+const User = model("user", userSchema)
+
+const joiUserRegisterSchema = Joi.object({
+    name: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    phone: Joi.number().phone().required(),
+    subscription: Joi.string().required(),
+    password: Joi.string()
+        .min(8)
+        .pattern(emailRegexp),
+    repeat_password: Joi.ref('password'),
+});
+
+const joiUserLoginSchema = Joi.object({
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+    password: Joi.string()
+        .min(8),
+});
+
+module.exports = {
+    User,
+    joiUserRegisterSchema,
+    joiUserLoginSchema
+}
