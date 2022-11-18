c86648a18cda04ab65fe9032ee3d16d7a6688147
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-11-18 17:25:52 +0200

    Update register.js

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -2,9 +2,9 @@ const { User } = require("../../models/user")
 const { createError } = require("../../services")
 const bcrypt = require("bcryptjs");
 const gravatar = require('gravatar');
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-const {SECRET_KEY} = process.env;
+// const {SECRET_KEY} = process.env;
 
 
 const register = async (req, res) => {
@@ -17,7 +17,7 @@ const register = async (req, res) => {
     const hashPassword = await bcrypt.hash(password, 15);
     const avatarURL = gravatar.url(email);
 
-    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+    // const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
     
     const result = await User.create({...req.body, password: hashPassword, avatarURL});
     res.status(201).json({
