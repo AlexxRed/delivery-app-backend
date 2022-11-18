19718d4fef3a57497de11a1c4b1fe742555ed5fb
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-11-18 17:51:46 +0200

    Update register.js

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -2,9 +2,9 @@ const { User } = require("../../models/user")
 const { createError } = require("../../services")
 const bcrypt = require("bcryptjs");
 const gravatar = require('gravatar');
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 
-// const {SECRET_KEY} = process.env;
+const {SECRET_KEY} = process.env;
 
 
 const register = async (req, res) => {
@@ -17,15 +17,21 @@ const register = async (req, res) => {
     const hashPassword = await bcrypt.hash(password, 15);
     const avatarURL = gravatar.url(email);
 
-    // const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
     
-    const result = await User.create({...req.body, password: hashPassword, avatarURL});
+    const result = await User.create({ ...req.body, password: hashPassword, avatarURL });
+    const createdUser = await User.findOne({ email });
+
+    const payload = {
+        id: createdUser._id
+    }
+
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+
+    const foundUser = await User.findByIdAndUpdate(user._id,{token})
+
     res.status(201).json({
-        user: {
-            email: result.email,
-            name: result.name,
-            phone: result.phone
-        }
+        ...foundUser,
+        token
     })
 }
 
