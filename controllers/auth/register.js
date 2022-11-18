d41f3179c536df6529822f1e75aec1f516282fca
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2022-11-18 17:14:24 +0200

    update

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -2,16 +2,23 @@ const { User } = require("../../models/user")
 const { createError } = require("../../services")
 const bcrypt = require("bcryptjs");
 const gravatar = require('gravatar');
+const jwt = require("jsonwebtoken");
+
+const {SECRET_KEY} = process.env;
+
 
 const register = async (req, res) => {
     const { email, password } = req.body;
     console.log(req.body);
     const user = await User.findOne({ email });
     if (user) {
-        throw createError(409, 'Email in use')
+        throw createError(409, 'Email is already in use')
     }
     const hashPassword = await bcrypt.hash(password, 15);
     const avatarURL = gravatar.url(email);
+
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+    
     const result = await User.create({...req.body, password: hashPassword, avatarURL});
     res.status(201).json({
         user: {
